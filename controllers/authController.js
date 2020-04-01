062b4a6579e2c972af18dddcb9457089add422c7
Brant Keener <bkeener@motiveretail.com>
2020-03-31 20:02:49 -0600

    Update secure settings for cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,13 +12,13 @@ const signToken = id => {
   })
 }
 
-const createAndSendToken = (user, statusCode, res) => {
+const createAndSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id)
   const cookieOptions = {
     expires: new Date(Date.now() + (process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000)),
-    httpOnly: true
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
   }
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
   res.cookie('jwt', token, cookieOptions)
   user.password = undefined
   res
@@ -42,7 +42,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   })
   const url = `${req.protocol}://${req.get('host')}/me`
   await new Email(newUser, url).sendWelcome()
-  createAndSendToken(newUser, 201, res)
+  createAndSendToken(newUser, 201, req, res)
 })
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -59,7 +59,7 @@ exports.login = catchAsync(async (req, res, next) => {
   }
 
   // 3) If everything is ok, send token to client
-  createAndSendToken(user, 200, res)
+  createAndSendToken(user, 200, req, res)
 })
 
 exports.logout = (req, res) => {
@@ -178,7 +178,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // 3) Update changedPasswordAt property for the user
 
   // 4) Log the user in, send JWT
-  createAndSendToken(user, 200, res)
+  createAndSendToken(user, 200, req, res)
 })
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -194,5 +194,5 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
   await user.save()
 
   // 4) Log user in (send new JWT)
-  createAndSendToken(user, 200, res)
+  createAndSendToken(user, 200, req, res)
 })
